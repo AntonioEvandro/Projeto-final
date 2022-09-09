e7b8683c16a1147f02ddec24d2925bfd5624a387
AntonioEvandro <100598553+AntonioEvandro@users.noreply.github.com>
2022-09-09 17:12:20 -0300

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
 import { useState } from 'react';
 import './App.css';
+import api from './services/api';
+import CepEncontrado from './components/CepEncontrado';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [cep, setCep] = useState([]);
+
+  const getCep = async (codCep) => {
+    const newCep = await api
+      .get(`${codCep.toString()}/json`)
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
+
+    //   console.log(newCep.data)
+    setCep([cep, newCep]);
+  };
+
+  const [codigoCep, setCodigoCep] = useState([]);
+  const mudar = (e) => {
+    setCodigoCep(e.target.value);
+  };
+
+  getCep(codigoCep);
+
+  console.log(cep);
 
   return (
     <div className="App">
       <h1>Buscar Cep</h1>
-      <div className="ceps">
-        <BuscarCep />
+
+      <div className="containerCep">
+        <form className="form">
+          <input
+            type="search"
+            value={codigoCep}
+            onChange={mudar}
+            className="search"
+            placeholder="Digite o CEP"
+            required
+          ></input>
+        </form>
+
+        <div className="busca">
+          {cep?.map((cod) => {
+            return <CepEncontrado cod={cod} />;
+          })}
+        </div>
       </div>
     </div>
   );
