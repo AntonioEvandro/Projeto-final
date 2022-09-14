aa32fd80ac051363a064527354b5a253d827f017
Antonio Evandro <100598553+AntonioEvandro@users.noreply.github.com>
2022-09-14 08:38:48 -0300

    css melhorado

--- src/components/BuscarCep.jsx
+++ src/components/BuscarCep.jsx
@@ -5,7 +5,7 @@ const BuscarCep = function (props) {
       <div className="cep">Código Postal: {props.cod?.cep}</div>
       <div className="endereco">{props.cod?.address}</div>
       <div className="distrito">Bairro/Distrito: {props.cod?.district}</div>
-      <div className="cidade">{props.cod?.city}</div>
+      <div className="cidade">Cidade: {props.cod?.city}</div>
       <div className="estado">UF: {props.cod?.state}</div>
       <div className="ddd">DDD: ({props.cod?.ddd})</div>
     </div>
